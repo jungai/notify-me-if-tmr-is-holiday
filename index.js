f216e5059a8c0979dcfecac87c2310dd265c481c
jungai <juworaphol125@gmail.com>
2021-08-14 15:22:08 +0700

    lol

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-import { format } from 'date-fns'
+import { isSameDay } from "date-fns";
+import { getDays, getBotToken, getBotId } from "./utils.mjs";
+import Discord from "discord.js";
+import "dotenv/config";
 
-console.log(format(new Date(), "'Today is a' eeee"))
+(async () => {
+  const currentDay = new Date();
+  const dayList = getDays();
+
+  const holiday = dayList.filter((day) => isSameDay(currentDay, day.date));
+  const isHoliday = holiday.length === 1;
+
+  if (!isHoliday) return;
+
+  //   connect to discord
+  const hook = new Discord.WebhookClient(getBotId(), getBotToken());
+
+  // embed things
+  const embed = new Discord.MessageEmbed()
+    .setTitle("พรุ่งนี้วันหยุด")
+    .setColor("#2ecc71")
+    .addFields({
+      name: "ชื่อวัน",
+      value: holiday[0].name,
+    });
+
+  // notify
+  await hook.send("แจ้งเตือน", { embeds: [embed] });
+
+  process.exit();
+})();
