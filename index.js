87cd42fb026bc5425ef1512370ad5123a29af9ee
jungai <32437056+jungai@users.noreply.github.com>
2021-08-14 17:26:51 +0700

    exit when is not holiday

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ import "dotenv/config";
   const holiday = dayList.filter((day) => isSameDay(currentDay, day.date));
   const isHoliday = holiday.length === 1;
 
-  if (!isHoliday) return;
+  if (!isHoliday) {
+    process.exit();
+
+    return;
+  };
 
   //   connect to discord
   const hook = new Discord.WebhookClient(getBotId(), getBotToken());
