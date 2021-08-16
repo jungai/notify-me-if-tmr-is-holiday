a8b1f17e83544f66cdb84fc06b8b0b2b5110c3b0
jungai <juworaphol125@gmail.com>
2021-08-17 01:27:53 +0700

    improve get env function

--- utils.mjs
+++ utils.mjs
@@ -8,10 +8,20 @@ export function normalizeData(days) {
   return days.map((day) => ({ ...day, date: new Date(day.date) }));
 }
 
+export function getEnv(name) {
+  const value = process.env[name]
+
+  if (!value) {
+    throw new Error(`env ${name} is required`)
+  }
+
+  return value
+}
+
 export function getBotId() {
-  return process.env.BOT_ID;
+  return getEnv("BOT_ID");
 }
 
 export function getBotToken() {
-  return process.env.BOT_TOKEN;
+  return getEnv("BOT_TOKEN");
 }
